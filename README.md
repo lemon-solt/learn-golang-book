f778b6be60a3d50706731680431ee381576c18f0
lemon-solt <engineer.piano.gu@gmail.com>
2023-05-20 23:01:10 +0900

    fix

---
+++ README.md
@@ -0,0 +1,10 @@
+# learn-golang-book
+
+
+# 参考
+『Goプログラミング実践入門』 例題
+https://github.com/mushahiroyuki/gowebprog
+
+
+# 疑問点等まとめ
+https://qiita.com/lemon-solt/items/ac2e938f727f4deb737e
--- main.go
+++ main.go
@@ -6,12 +6,11 @@ import (
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintln(writer, "vagrant golang %s")
+	fmt.Fprint(writer, "vagrant golang %s", request.URL.Path[1:])
 }
 
 func main() {
 	fmt.Println("start server")
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":7000", nil)
-
 }
